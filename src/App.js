d2854926a3226c47a840e761d9810a13b918ab77
Tomás <jtml.mass@gmail.com>
2021-03-22 21:06:18 -0400

    Editing some variables to show goal

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div class="h-64">
+    <div class="h-3/4">
         <div class="p-4 m-4 bg-green-600">
-            <h1 class="text-2xl font-bold text-white">Tailwind CSS Demo</h1>
+            <h1 class="text-2xl font-bold text-white">Curriculum Vitae - Tomas</h1>
         </div>
-        <div class="p-4 m-4 bg-green-300 h-full">
-            <h2 class="text-green-900">Have much fun using Tailwind CSS</h2>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">My Tailwind Button</button>
+        <div class="p-4 m-4 bg-green-100 h-1/2">
+            <h2 class="text-green-900">Page with the goal of showing CV</h2>
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Button</button>
         </div>  
     </div>
   );
